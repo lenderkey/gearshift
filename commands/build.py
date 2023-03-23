ae37db59cea1489029d93e5ee75aba545212d547
David Janes <davidjanes@gmail.com>
2023-03-23 17:29:04 -0400

    reorg into helpers

--- commands/build.py
+++ commands/build.py
@@ -8,66 +8,14 @@
 
 from Context import Context
 
-import hashlib
-import os
 import logging as logger
 import pprint
 
 import click
 
+import helpers
+
 L = "build"
-
-
-def sha256_file(fd):
-    sha256 = hashlib.sha256()
-    while True:
-        data = fd.read(65536)  # Read 64KB at a time
-        if not data:
-            break
-        sha256.update(data)
-
-    return sha256.hexdigest()
-
-def md5_data(*av):
-    md5 = hashlib.md5()
-    for item in av:
-        data = str(item).encode("utf-8")
-        md5.update(data)
-        md5.update(b"@@")
-    return md5.hexdigest()
-
-def walker():
-    root = Context.instance.src_root_path
-    for folder, dirs, files in os.walk(root):
-        for filename in files:
-            filename = os.path.join(folder, filename)
-            filename = os.path.relpath(filename, root)
-
-            yield filename
-
-def analyze(filename):
-    fullpath = os.path.join(Context.instance.src_root_path, filename)
-    stbuf = os.stat(fullpath)
-
-    try:
-        with open(fullpath, "rb") as fin:
-            """Make SHA256 hash of file context"""
-
-            return {
-                "filename": filename,
-                "nhash": md5_data(filename),
-                "ahash": md5_data(stbuf.st_ino, stbuf.st_size, stbuf.st_mtime),
-                "fhash": sha256_file(fin),
-            }
-    except IOError:
-        logger.warning(f"{L}: cannot open {fullpath}")
-
-        return {
-            "filename": filename,
-            "nhash": md5_data(filename),
-            "ahash": None,
-            "fhash": None,
-        }
 
 @cli.command("build", help="Build a Gearshift Database") # type: ignore
 @click.option("--quit-on-error/--no-quit-on-error", is_flag=True)
@@ -78,5 +26,5 @@ def build(quit_on_error, csv):
 
     cursor = context.cursor()
 
-    for filename in walker():
-        pprint.pprint(analyze(filename))
+    for filename in helpers.walker():
+        pprint.pprint(helpers.analyze(filename))
--- helpers.py
+++ helpers.py
@@ -6,10 +6,15 @@
 #   2023-03-23
 #
 
-from typing import Union, Any
-import re
+from typing import BinaryIO, Union, Any
 
-def _list_advance(o, keypath:str, required:bool, key=None):
+import re
+import hashlib
+import os
+
+import logging as logger
+
+def _list_advance(o, keypath:str, required:bool, key:str=None) -> Any:
     while isinstance(o, list):
         if len(o) == 0 and required:
             raise KeyError(keypath)
@@ -20,7 +25,7 @@ def _list_advance(o, keypath:str, required:bool, key=None):
 
     return o
 
-def set(d, keypath:str, value:object):
+def set(d:dict, keypath:str, value:object) -> None:
     parts = keypath.split(".")
     for key in parts[:-1]:
         ## d = _list_advance(d, keypath, required=False, key=key)
@@ -33,7 +38,7 @@ def set(d, keypath:str, value:object):
     key = parts[-1]
     d[key] = value
 
-def get(d, keypath:str, default:Any=None, required:bool=False, first:bool=True) -> Any:
+def get(d:dict, keypath:str, default:Any=None, required:bool=False, first:bool=True) -> Any:
     assert isinstance(required, bool)
 
     parts = keypath.split(".")
@@ -64,3 +69,61 @@ def get(d, keypath:str, default:Any=None, required:bool=False, first:bool=True) 
         raise KeyError(keypath)
     else:
         return default
+
+
+def sha256_file(fd:BinaryIO) -> str:
+    sha256 = hashlib.sha256()
+    while True:
+        data = fd.read(65536)  # Read 64KB at a time
+        if not data:
+            break
+        sha256.update(data)
+
+    return sha256.hexdigest()
+
+def md5_data(*av) -> str:
+    md5 = hashlib.md5()
+    for item in av:
+        data = str(item).encode("utf-8")
+        md5.update(data)
+        md5.update(b"@@")
+    return md5.hexdigest()
+
+def walker():
+    from Context import Context
+
+    root = Context.instance.src_root_path
+    for folder, dirs, files in os.walk(root):
+        for filename in files:
+            filename = os.path.join(folder, filename)
+            filename = os.path.relpath(filename, root)
+
+            yield filename
+
+def analyze(filename:str) -> dict:
+    L = "helpers.analyze"
+    
+    from Context import Context
+    
+    fullpath = os.path.join(Context.instance.src_root_path, filename)
+    stbuf = os.stat(fullpath)
+
+    try:
+        with open(fullpath, "rb") as fin:
+            """Make SHA256 hash of file context"""
+
+            return {
+                "filename": filename,
+                "nhash": md5_data(filename),
+                "ahash": md5_data(stbuf.st_ino, stbuf.st_size, stbuf.st_mtime),
+                "fhash": sha256_file(fin),
+            }
+    except IOError:
+        logger.warning(f"{L}: cannot open {fullpath}")
+
+        return {
+            "filename": filename,
+            "nhash": md5_data(filename),
+            "ahash": None,
+            "fhash": None,
+        }
