ccecdf9bcd6532254f1c8604fb1c7f44de7b6811
David Janes <davidjanes@gmail.com>
2023-08-30 12:55:53 -0230

    files are now put to the database

--- bl/pushed_zip.py
+++ bl/pushed_zip.py
@@ -4,15 +4,22 @@ import io
 
 from Context import Context
 import bl
+import db
 
 def pushed_zip(context:Context, raw_data:bytes) -> dict:
     raw_io = io.BytesIO(raw_data)
+
+    db.setup()
 
     zipper = zipfile.ZipFile(raw_io, mode="r")
     for dst_name in zipper.namelist():
         data = zipper.read(dst_name)
         in_item = bl.data_analyze(context, dst_name, data=data)
-        is_written = bl.data_ingest(context, in_item, data=data)
+        bl.data_ingest(context, in_item, data=data)
+
+        db.start()
+        db.put_record(in_item)
+        db.commit()
 
         print("RECEIVED", in_item)
 
